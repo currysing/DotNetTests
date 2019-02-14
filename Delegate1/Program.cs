7d9de1944080d31650afa1dfef26b985c18a2ce4
Kevin <currysing@gmail.com>
2019-02-14 16:23:25 +0800

    1st commit

--- Delegate1/Program.cs
+++ Delegate1/Program.cs
@@ -14,6 +14,7 @@ namespace WinformTest
 		[STAThread]
 		static void Main()
 		{
+			// add some comment line by user1
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
